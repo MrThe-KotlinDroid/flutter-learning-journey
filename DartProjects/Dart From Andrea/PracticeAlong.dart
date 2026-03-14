1558acd5b5de4b0d3ac4ceb85002887d477d42c5
Abrar Hamim Sahil <abrarhamim2@gmail.com>
2026-03-15 02:42:28 +0600

    feat: add M4P2.dart for arithmetic operations using enum and refactor PracticeAlong.dart to demonstrate enum usage

--- DartProjects/Dart From Andrea/PracticeAlong.dart
+++ DartProjects/Dart From Andrea/PracticeAlong.dart
@@ -1,12 +1,19 @@
-void main() {
-  var i = 1;
-  while (i <= 5) {
-    print('x' * i);
-    i++;
-  }
-  print('Done');
+enum Medal { gold, silver, bronze, noMedal }
 
-  for (var i in [1, 2, 4]) {
-    print(i);
+void main() {
+  Medal medal = Medal.bronze;
+  print(medal);
+  print(medal.name);
+  print(Medal.values);
+  print(Medal.values.byName('bronze'));
+  switch (medal) {
+    case Medal.gold:
+      print('gold');
+    case Medal.silver:
+      print('silver');
+    case Medal.bronze:
+      print('bronze');
+    case Medal.noMedal:
+      print('no medal, try again');
   }
 }
---
+++ Ostad/class_class.dart
@@ -0,0 +1,14 @@
+//Class On Module 5 C1
+//Class about -> 'OOP Foundations'
+
+class Student {
+  String name;
+  String studentId;
+  double gpa;
+
+  Student({
+    required this.name,
+    required this.studentId,
+    required this.gpa,
+  });
+}
