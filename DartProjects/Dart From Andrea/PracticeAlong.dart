5712ddd0cbe3326214ad924b87336d4cc3d90690
Md. Abrar Hamim <abrarhamim2@gmail.com>
2026-04-24 22:57:32 +0600

    feat: Learned null safety half way

--- DartProjects/Dart From Andrea/PracticeAlong.dart
+++ DartProjects/Dart From Andrea/PracticeAlong.dart
@@ -1,10 +1,10 @@
-void main() {
-  final list = [1, 2, 3];
-  final copy1 = list;
-  copy1[0] = 5;
-  final copy2 = [...list];
-  copy2[0] = 6;
-  print('list: $list');
-  print('copy1: $copy1');
-  print('copy2: $copy2');
+main() {
+  int x = -1;
+  int? maybeValue;
+  if (x > 0) {
+    maybeValue = x;
+  }
+  maybeValue ??= 0;
+  int value = maybeValue;
+  print(value);
 }
