7cc32e4f93f87788902b4cd0604870a65afee45d
Md. Abrar Hamim <abrarhamim2@gmail.com>
2026-05-31 00:23:47 +0600

    feat: implement Account class with deposit and withdraw methods; add where and firstWhere functions

--- DartProjects/Dart From Andrea/PracticeAlong.dart
+++ DartProjects/Dart From Andrea/PracticeAlong.dart
@@ -1,27 +1,26 @@
+class Account {
+  double balance = 0;
+
+  void deposit(double amount) {
+    balance += amount;
+  }
+
+  bool withdraw(double amount) {
+    if (amount <= this.balance) {
+      balance -= amount;
+      return true;
+    } else
+      return false;
+  }
+}
+
 void main() {
-  const list = [1, 2, 3];
-  const l2 = [1.0, 2.0, 3.0];
-  list.forEach((value) => print(value));
-  // final doubles = list.map((value) => value * 2).toList();
-  // final double = doubleItems(list);
-  final doubles = transform<int>(list, (x) => x * 2);
-  final sqaures = transform<double>(l2, (x) => x * x);
-  print(doubles);
-  print(sqaures);
-}
+  var account1 = Account();
+  account1.deposit(100);
+  print(account1.balance);
+  account1.deposit(50);
+  print(account1.balance);
 
-List<T> transform<T>(List<T> items, T Function(T) f) {
-  var result = <T>[];
-  for (var x in items) {
-    result.add(f(x));
-  }
-  return result;
-}
-
-List<int> doubleItems(List<int> items) {
-  var result = <int>[];
-  for (var x in items) {
-    result.add(x * 2);
-  }
-  return result;
+  print(account1.withdraw(20));
+  print(account1.withdraw(2000));
 }
